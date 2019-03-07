bbba48b8d290edf0c369fd2aac388114825d6a57
Deepak Bawa <bawa_d@ymail.com>
2019-03-07 13:45:12 +0530

    Create app.module.ts

---
+++ app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { NgxElectronModule } from 'ngx-electron';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OneComponent,
+    TwoComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxElectronModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
